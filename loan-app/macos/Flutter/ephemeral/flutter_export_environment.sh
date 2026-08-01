e6fdd1ab75557ea219cc980428541d0b8287e859
saurabh <saurabhmvp01@gmail.com>
2026-08-01 14:57:54 +0530

    jdjd

--- loan-app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ loan-app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vinaymathure/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vinaymathure/loan-web/loan-app"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/vinaymathure/loan-web/loan-app/macos/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_ROOT=D:\flutter_windows_3.35.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\loan-web\loan-web\loan-app"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\loan-web\loan-web\loan-app\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
